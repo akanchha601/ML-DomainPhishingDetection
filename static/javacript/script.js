e18eb4c629bdd7658ce41d764d296204b6a71bee
akanchha601 <55644004+akanchha601@users.noreply.github.com>
2020-06-04 20:19:10 -0400

    Update script.js

--- static/javacript/script.js
+++ static/javacript/script.js
@@ -1,9 +1,10 @@
-/*This action method calls on submit button to response the user input request and shows the result on screen */
-('#idform').on('submit', function () {
-	$.getJSON('/background_process', {
+/*This action method calls on submit button to response the user input request and shows the result on screen*/
+('#idform').on('submit', function (){
+	$.getJSON('/application_process', {
 	    domain: $('input[name="domain"]').val(),
-		}, function(data) {
-		$("#result").text(data.result);
+		}, function(inputData) {
+		$("#result").text(inputData.result);
 		});
 		return false;
 		});
+
