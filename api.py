a526ac1da0c9b57528d4758c81bb5e0038a09c75
akanchha601 <55644004+akanchha601@users.noreply.github.com>
2020-06-04 20:20:32 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,44 +1,37 @@
 from flask import Flask, render_template, request, jsonify
-import glob,os
 import ssl,socket
-import OpenSSL.crypto as crypto
-from cryptography.hazmat._oid import ObjectIdentifier
 from asn1crypto.x509 import Certificate
 from cryptography.hazmat.backends import default_backend
-from OpenSSL.crypto import (load_certificate, dump_privatekey, dump_certificate, X509, X509Name, PKey)
-from datetime import datetime
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import mutual_info_score
-from sklearn.metrics.pairwise import euclidean_distances
-from scipy.stats import ks_2samp
-import urllib.request, sys, re
+from OpenSSL.crypto import (load_certificate, X509)
 from publicsuffix2 import PublicSuffixList
-import xmltodict, json
-import numpy as np 
-import re,csv
-import urllib, sys
-import math
-app = Flask(__name__)
+import xmltodict, json, re
+import csv
+import urllib.request, sys, xmltodict, math
+from datetime import datetime
+
+
+
+application = Flask(__name__)
+# Global variables
 global domain,cert
 domain=None
 cert=None
 
 
 #This certificate extraction method is called for extracting SSL certificate from a domain.
-#Ssl - TLS/SSL wrapper for socket objects. (n.d.). Retrieved April 29, 2020, from https://docs.python.org/3/library/ssl.html
+#This code is a modified version of the code to retrieve SSL certificates found on: https://docs.python.org/3/library/ssl.html written by Python Software Foundation
 def readDomain(domain):
     host = domain
     port= 443
     dst = (host,port)
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error:
         return "Error in socket connection"
     except ValueError:
         return "Please enter valid site name"    
     try:
-        s.connect(dst)
+        conn.connect(dst)
     except socket.gaierror:
         return "Address-related error connecting to server" 
         sys.exit(1)
@@ -49,9 +42,8 @@ def readDomain(domain):
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
-    s = ctx.wrap_socket(s, server_hostname=dst[0])
-    cert_bin = s.getpeercert(True)
-    cert = ssl.DER_cert_to_PEM_cert(s.getpeercert(True))
+    conn = ctx.wrap_socket(conn, server_hostname=dst[0])
+    cert = ssl.DER_cert_to_PEM_cert(conn.getpeercert(True))
     return cert
 
 #This feature method checks whether the certificate is domain validated or not. In any domain validated certificate, the subject name must not have a business category and organization name fields.
@@ -68,7 +60,7 @@ def isdomainvalidated(inputDomain):
         Is_domian_validated=True  
     return Is_domian_validated
 
-#This feature method checks whether the issuer component had state field or not in the certificate
+#This feature method checks whether the issuer component has state field or not in the certificate
 def issuerHasState(inputDomain):
     cert=readDomain(inputDomain)
     x509_One=crypto.load_certificate(crypto.FILETYPE_PEM, cert)
@@ -79,7 +71,7 @@ def issuerHasState(inputDomain):
         IssuerHasState=False
     return IssuerHasState
 
-#This feature method calculates number of details, number of characters of both Issuer and subject components and also number of extensions contained in the certificate.
+#This feature method calculates number of details in subject component, number of characters in Issuer and subject components and also number of extensions present in the certificate.
 def subIsslength(inputDomain):
     subject = []
     cert=readDomain(inputDomain)
@@ -94,7 +86,7 @@ def subIsslength(inputDomain):
     total=0
     total1=0
     for word in subject:
-        total += len(str(word))
+        total+= len(str(word))
     for word in issuer:
         total1 += len(str(word))
 	# Calculates number of details present in subject component
@@ -109,10 +101,11 @@ def subIsslength(inputDomain):
 
 
 #Ranking of a domain. The ranking is analyzed by estimating traffic of the domain.
-#Python One-liner to get your site's Alexa Rank. Retrieved April 29, 2020, from https://gist.github.com/masnun/3170870
+#This code is used verbatim to meet the requirements of the functionality of our application from the code to get Alexa Rank of domain found on: https://gist.github.com/masnun/3170870 written by risheek20(25 September 2018)
 def domainRanking(inputDomain):
     host=inputDomain
-    xml = urllib.request.urlopen('http://data.alexa.com/data?cli=10&dat=s&url={}'.format(host)).read()
+    url_link='http://data.alexa.com/data?cli=10&dat=s&url={}'
+    xml = urllib.request.urlopen(url_link.format(host)).read()
     result= xmltodict.parse(xml)
     data = json.dumps(result).replace("@","")
     data_tojson = json.loads(data)
@@ -160,8 +153,8 @@ def daysValidity(inputDomain)
     return DaysValidity
 	
 	
-#This decision rules method claasifies the domain as a legitimate or phishing.
-def decisionRules(feature_1,feature_2,feature_3,feature_4,feature_5,feature_6,feature_7,feature_8,feature_9,feature_10,feature_11):
+#This decision rules method classifies the domain as a legitimate or phishing.
+def decisionRules(feature_1,feature_2,feature_3,feature_4,feature_5,feature_6,feature_7,feature_8,feature_9,feature_10):
     output=None
     myDict = {'Is_domian_validated':feature_1,'Issuer_is_com':feature_9,'DaysValidity':feature_10,'Subject_is_com':feature_8
         ,'IssuerElements':feature_5,'Domain_ranking':feature_7,
@@ -170,38 +163,38 @@ def decisionRules(feature_1,feature_2,feature_3,feature_4,feature_5,feature_6,fe
         output='Phishing Domain'
     elif(myDict['Domain_ranking']<=42089 and myDict['Is_domian_validated']==False and myDict['IssuerHasState']==False and myDict['Subject_is_com']==True):
         output='Legitimate Domain'
-	elif(myDict['Domain_ranking']<=42089 and myDict['Is_domian_validated']==False and myDict['IssuerHasState']==True and myDict['SubjectElements']<=5):
+    elif(myDict['Domain_ranking']<=42089 and myDict['Is_domian_validated']==False and myDict['IssuerHasState']==True and myDict['SubjectElements']<=5):
         output='Phishing Domain'
-	elif(myDict['Domain_ranking']<=42089 and myDict['Is_domian_validated']==False and myDict['IssuerHasState']==True and myDict['SubjectElements']>5):
+    elif(myDict['Domain_ranking']<=42089 and myDict['Is_domian_validated']==False and myDict['IssuerHasState']==True and myDict['SubjectElements']>5):
         output='Legitimate Domain'
-	elif(myDict['Domain_ranking']<=42089 and myDict['Is_domian_validated']==False and myDict['IssuerHasState']==False and myDict['Subject_is_com']==False and myDict['SubjectLength']<=66):
+    elif(myDict['Domain_ranking']<=42089 and myDict['Is_domian_validated']==False and myDict['IssuerHasState']==False and myDict['Subject_is_com']==False and myDict['SubjectLength']<=66):
         output='Legitimate Domain'
-	elif(myDict['Domain_ranking']<=42089 and myDict['Is_domian_validated']==False and myDict['IssuerHasState']==False and myDict['Subject_is_com']==False and myDict['SubjectLength']>66):
+    elif(myDict['Domain_ranking']<=42089 and myDict['Is_domian_validated']==False and myDict['IssuerHasState']==False and myDict['Subject_is_com']==False and myDict['SubjectLength']>66):
         output='Phishing Domain'
-	elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==False):
+    elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==False):
         output='Phishing Domain'
-	elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==True and myDict['IssuerHasState']==False and myDict['IssuerElements']<=3):
+    elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==True and myDict['IssuerHasState']==False and myDict['IssuerElements']<=3):
         output='Legitimate Domain'
-	elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==True and myDict['IssuerHasState']==False and myDict['IssuerElements']>3 and myDict['SubjectLength']>16):
+    elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==True and myDict['IssuerHasState']==False and myDict['IssuerElements']>3 and myDict['SubjectLength']>16):
         output='Phishing Domain'
-	elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==True and myDict['IssuerHasState']==False and myDict['IssuerElements']>3 and myDict['SubjectLength']<=16 and myDict['DaysValidity']>735):
+    elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==True and myDict['IssuerHasState']==False and myDict['IssuerElements']>3 and myDict['SubjectLength']<=16 and myDict['DaysValidity']>735):
         output='Legitimate Domain'	
-	elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==True and myDict['IssuerHasState']==False and myDict['IssuerElements']>3 and myDict['SubjectLength']<=16 and myDict['DaysValidity']<=735):
+    elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==True and myDict['IssuerHasState']==False and myDict['IssuerElements']>3 and myDict['SubjectLength']<=16 and myDict['DaysValidity']<=735):
         output='Phishing Domain'
-	elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==True and myDict['IssuerHasState']==True and myDict['ExtensionNumber']>10):
+    elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==True and myDict['IssuerHasState']==True and myDict['ExtensionNumber']>10):
         output='Legitimate Domain'
     elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==True and myDict['IssuerHasState']==True and myDict['ExtensionNumber']<=10 and myDict['Issuer_is_com']==False):
         output='Phishing Domain'
     elif(myDict['Is_domian_validated']==True and myDict['Subject_is_com']==True and myDict['IssuerHasState']==True and myDict['ExtensionNumber']<=10 and myDict['Issuer_is_com']==True):
         output='Phishing Domain'
-	else:
+    else:
         output='Please try again'
         
     return output
 
 #Running the background process of Web API by calling feature creation methods, certificate extection method and decision rules method.
-@app.route('/background_process')
-def background_process():
+@app.route('/application_process')
+def application_process():
     try:
         domainInput = request.args.get('domain', 0, type=str)
         if domainInput == '':
@@ -209,11 +202,11 @@ def background_process():
         else:
             domain=domainInput
             try:
-			    #calling certificate extraction method to obtain entered domain's SSL certificate.
+			#calling certificate extraction method to obtain entered domain's SSL certificate.
                 cert=readDomain(domain)
             except ValueError:
                 return jsonify(result='Please enter valid site name') 
-			#calling feature creation methods to create features.
+		#calling feature creation methods to create features.
             feature_1=isdomainvalidated(domain)
             feature_2=issuerHasState(domain)
             feature_3,feature_4,feature_5,feature_6=subIsslength(domain)
@@ -221,15 +214,15 @@ def background_process():
             feature_8=subDotCom(domain)
             feature_9=issDotCom(domain)
             feature_10=daysValidity(domain)
-			#calling decision rules method to claasify the domain as a legitimate or phishing
+		#calling decision rules method to classify the domain as a legitimate or phishing
             decision_rule= decisionRules(feature_1,feature_2,feature_3,feature_4,feature_5,feature_6,feature_7,feature_8,feature_9,feature_10)
             return jsonify(result=decision_rule) 
     except Exception as e:
         return str(e)
-@app.route('/',methods = ['POST', 'GET'])
-def get_output():
+@application.route('/',methods = ['POST', 'GET'])
+def application_output():
     return render_template('api.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
